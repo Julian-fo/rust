a8b2d4ce5c63817751b1dd6070259e946cf6d148
julian.easy.life <julian.easy.life@gmail.com>
2025-02-17 09:02:04 +0000

    up

--- src/bin/lifetime.rs
+++ src/bin/lifetime.rs
@@ -1,15 +1,30 @@
+use std::fmt::Display;
 
 fn main() {
     let s1 = String::from("hello world");
     let s2 = String::from("hello rust");
     let s3 = longest_string(&s1, &s2);
     println!("The longest string is {}", s3);
+
+    println!("The longest string is {}", longest_announcement("hello world", "hello rust", "rust"));
 }
 
-fn longest_string<`a>(s1: &`a str, s2: &`a str) -> &`a str {
+fn longest_string<'a>(s1: &'a str, s2: &'a str) -> &'a str {
     if s1.len() > s2.len() {
         s1
     } else {
         s2
+    }
+}
+
+fn longest_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display + std::fmt::Debug,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
